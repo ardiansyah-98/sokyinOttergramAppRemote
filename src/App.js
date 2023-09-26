a15e21245f896a81c0ea617f245fddfa6b2b0559
ardiansyah-98 <syahsyah9898@gmail.com>
2023-09-26 15:44:02 +0800

    Modify App.js and App.css

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ardian Was Here
         </a>
       </header>
     </div>
